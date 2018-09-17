5bc28944133121bea57b3fc596327267f79f8a6d
Kurt Daill <Daill.Kurt@gmail.com>
2018-09-17 10:54:09 -0500

    GameEngine First Draft

---
+++ NicholasKurtTTT/app/src/main/java/com/example/kdail_000/nicholas_kurt_ttt/GameEngine.java
@@ -0,0 +1,8 @@
+package java.com.example.kdail_000.nicholas_kurt_ttt;
+
+/**
+ * Created by kdail_000 on 9/16/2018.
+ */
+
+public class GameEngine {
+}
---
+++ NicholasKurtTTT/app/src/test/java/com/example/kdail_000/nicholas_kurt_ttt/GridTest.java
@@ -0,0 +1,8 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by kdail_000 on 9/17/2018.
+ */
+class GridTest {
+
+}
